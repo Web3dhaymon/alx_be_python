95f3cd8ca8e767675c8a2d31c43c20a8d0db4da3
Web3 dhaymon <ibnkc2@gmail.com>
2025-06-07 17:00:54 +0100

    arithmetic_operations.py modified

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,10 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            return "Error: Division by zero"
-        return num1 / num2
+            return "Cannot divide by zero"
+        else:
+            return num1 / num2
     else:
-        return "Error: Invalid operation"
+        return "Invalid operation"
+
 
