751ab69b4523ce42c15f14878ff0dc0cbab9cf92
Nightwing <christianchinedu567@gmail.com>
2025-11-19 01:08:03 +0100

    Basic operations

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+       # arithemetic Operations
+sum = number1 + number2    
+diffrence = number1 - number2  
+product = number2 * number1
+
+            #Output
+print(f"Addition of {number1} and {number2} is = {sum}")
+print(f" Subtraction of {number2} from {number1} is {diffrence}")
+print(f"Multiplication of {number1} and {number2} is {product}")
