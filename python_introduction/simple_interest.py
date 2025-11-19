90f97e13043c9a2117c5c93afd680814e00333f9
Nightwing <christianchinedu567@gmail.com>
2025-11-19 01:48:00 +0100

    updated code

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,9 @@
-# Define variables
+       ### assign Variables
 principal = 1000
 rate = 0.05
 time = 3
 
-# Calculate simple interest
-interest = principal * rate * time
+      #### Calculate simple interest
+Intrest = (principal * rate * time)
 
-# Print result
-print("The simple interest is:", interest)
+print(f"The simple interest is {Intrest}")
