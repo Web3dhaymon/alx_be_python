a4c5e3cc99baf88851964353e0c2c0d379eb0240
Nightwing <christianchinedu567@gmail.com>
2025-11-19 01:57:03 +0100

    Update code

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,6 +4,6 @@ rate = 0.05
 time = 3
 
       #### Calculate simple interest
-interest = (principal * rate * time)
+interest = principal * rate * time 
 
 print(f"The simple interest is {interest}")
